597a1dbb47fdb67f615bc5d3d485c070507cf2f3
Wxcafé (Clément Hertling) <wxcafe@wxcafe.net>
2018-04-11 17:19:45 +0200

    working first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Hackers Bot
+
+Posts hackers gifs on twitter and mastodon.
+You need to register a twitter app and a mastodon app for it to work correctly,
+and fill the variables in the hackers_gif.py file. Install dependencies via
+`pip3 install -r requirements.txt`.
+
+The gifs are possibly subject to copyright from whoever produced the movie, but
+`¯\_(ツ)_/¯`
+
+Thanks to @lindseybieda (https://twitter.com/lindseybieda) for the code needed
+to generate the gifs (https://github.com/LindseyB/videos-to-gif)
---
+++ hackers-gifs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os, random
+from mastodon import Mastodon
+import tweepy
+
+tw_consumer_key = ""
+tw_consumer_secret = ""
+tw_access_token = ""
+tw_access_token_secret = ""
+
+mastodon_url = 'https://botsin.space'
+
+tw_auth = tweepy.OAuthHandler(tw_consumer_key,tw_consumer_secret)
+tw_auth.set_access_token(tw_access_token, tw_access_token_secret)
+twitter = tweepy.API(tw_auth)
+
+masto = Mastodon(client_id='app_tokens', access_token='log_in', api_base_url=mastodon_url)
+
+gif_file = "./gifs/" + random.choice(os.listdir("./gifs/"))
+
+gif = masto.media_post(gif_file)
+
+masto.status_post(status="﻿  ", \
+        media_ids=[gif], \
+        visibility='unlisted',
+        sensitive=None)
+
+twitter.update_with_media(gif_file)
